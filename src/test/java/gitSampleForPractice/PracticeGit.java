d30cf8a9f3792c7967258a1d8e00f1c3732f6995
AdarshaLS <adarsha1984@gmail.com>
2022-03-13 08:23:25 +0530

    this is commit from Asian time

---
+++ src/test/java/gitSampleForPractice/PracticeGit.java
@@ -0,0 +1,22 @@
+package gitSampleForPractice;
+
+import org.testng.annotations.Test;
+
+public class PracticeGit {
+	
+	@Test 
+	public void practiceGit()
+	{
+		System.out.println("My name is mark");
+		System.out.println("My name is Joji");
+		System.out.println("My name is Joseph");
+		
+	}
+	
+	@Test
+	public void practiceSample()
+	{
+		System.out.println("My work testing");
+	}
+
+}
---
+++ src/test/java/gitSampleForPractice/PracticeWell.java
@@ -0,0 +1,23 @@
+package gitSampleForPractice;
+
+import org.testng.annotations.Test;
+
+public class PracticeWell {
+	
+	@Test 
+	public void practiceWell()
+	{
+		System.out.println("My name is mark");
+		System.out.println("My name is Joji");
+		System.out.println("My name is Joseph");
+		
+	}
+	
+	@Test
+	public void practiceWhen()
+	{
+		System.out.println("My work testing");
+		System.out.println("And Automation");
+	}
+
+}
